7692e980a048dd86cadd0f4b9dd005a25921af2b
Christian Nagel <christian@christiannagel.com>
2017-06-12 17:07:31 +0200

    unit testing sample, web app with docker sample

---
+++ LibSample/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string expected = "abc";
+            var s = new LibSample.Sample();
+            string result = s.ATest("ABC");
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
